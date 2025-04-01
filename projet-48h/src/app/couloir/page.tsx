99570ad8a2b4f2f15c622bb0752c60a672a99be7
MattDoney <matthis.doney@gmail.com>
2025-04-01 09:04:31 +0200

    couloir pas fini

--- projet-48h/src/app/couloir/page.tsx
+++ projet-48h/src/app/couloir/page.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 
 export default function SalleDeBain() {
     return (
-        <div className="h-screen w-screen bg-[url(/assets/image/salle-de-bain.png)] bg-no-repeat bg-contain bg-center bg-[#D5D5D5]"></div>
+        <div className="h-screen w-screen bg-[url(/assets/image/couloir.png)] bg-no-repeat bg-contain bg-center bg-[#D5D5D5]">
+
+            <a   href="/salle-de-bain"  className="absolute w-[90px] h-[60px] right-[32%] top-[1%] cursor-pointer hover:bg-red-600 opacity-20"
+            />
+        </div>
     );
-  }
+}
