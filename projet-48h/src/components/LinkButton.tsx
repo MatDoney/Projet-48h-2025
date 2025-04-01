1db393b5e6eb333e861c197ed0d560118d15bce6
mstrndrsn <noteo.gaming@protonmail.com>
2025-04-01 10:58:30 +0200

    feat: add custom link component

---
+++ projet-48h/src/components/LinkButton.tsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+interface ILinkProps {
+  href: string
+  className: string
+  title: string
+}
+
+export function LinkButton(props: ILinkProps) {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [visible, setVisible] = useState(true);
+
+  const handleMouseMove = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    const rect = e.currentTarget.getBoundingClientRect();
+    setPosition({ x: e.clientX - rect.left, y: e.clientY - rect.top });
+  };
+
+  return (
+      <a
+        href={props.href}
+        className={`cursor-pointer hover:bg-red-600 opacity-50 ${props.className}`}
+        onMouseMove={handleMouseMove}
+        onPointerEnter={() => setVisible(true)}
+        onPointerLeave={() => setVisible(false)}
+      >
+        {visible && (
+          <div
+            className="absolute bg-black text-white px-2 py-1 rounded shadow-lg flex items-center justify-center w-3xs"
+            style={{
+              left: `${position.x}px`,
+              top: `${position.y}px`,
+              transform: "translate(10px, 10px)",
+            }}
+          >
+          {props.title}
+          </div>
+        )}
+      </a>
+  );
+}
+
